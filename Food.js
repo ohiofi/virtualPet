f9904bc5b303f97c1fdeca03facf1ac7957393e1
Sakura <owenkaufman94@gmail.com>
2026-02-04 10:17:11 -0500

    more food changes
    stop updating food or draw 25

--- Food.js
+++ Food.js
@@ -5,17 +5,16 @@
     Name:  The name of the food ("Pizza" for example)
     Value: How much hunger the pet restores from eating the food
     Eaten: Tells if the food has already been consumed (Usually changed by the Eat method)
-
 */
 
+//Hopefully this is enough with the information that i have
 function Food(type, name, value) {
-    //i do not like JS objects. i have no idea if this will actually run cuz its so confusing
     this.Type  = type  || "None"
     this.Name  = name  || "NewFood" //quick pretend this is readable
     this.Value = value || 0 
     this.Eaten = false
 
-    //Makes the pet eat the food, returns the food's value or 0 if it's already eaten
+    //makes the pet eat the food, returns the food's value or 0 if it's already eaten
     this.Eat = function() { //oh god please help me this better not all be in here
         if (this.Eaten) {
             return 0
