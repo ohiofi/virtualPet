a1b7c934ef8d36596de913058a0d64ee865aac99
ohiofi <20hz20khz@gmail.com>
2026-02-05 12:00:56 -0500

    Update Food.js using ES6 format
    Didn't change any of the code structure. I just feel like ES6 classes are easier to read.

--- Food.js
+++ Food.js
@@ -1,49 +1,47 @@
 /*
-    Heres what all these variables do exactly:
+    Heres what all these attributes/instance variable do exactly:
     
-    Type:  The type of food (Food, Drink, etc)
-    Name:  The name of the food ("Pizza" for example)
-    Value: How much hunger the pet restores from eating the food
-    Eaten: Tells if the food has already been consumed (Usually changed by the Eat method)
+    type:  The type of food (Food, Drink, etc)
+    name:  The name of the food ("Pizza" for example)
+    value: How much hunger the pet restores from eating the food
+    eaten: Tells if the food has already been consumed (Usually changed by the eat method)
 */
 
-//Hopefully this is enough with the information that i have
 class Food{
     constructor(type, name, value) {
-        this.Type  = type  || "None"
-        this.Name  = name  || "NewFood" //quick pretend this is readable
-        this.Value = value || 0 
-        this.Eaten = false
+        this.type  = type  || "None";
+        this.name  = name  || "NewFood"; //quick pretend this is readable
+        this.value = value || 0;
+        this.eaten = false;
     }
 
-    //wow this is so easily readable and im not completely misunderstanding the entire concept of code readability
-    setType  = function(newType)  { this.Type  = newType  }
-    setName  = function(newName)  { this.Name  = newName  }
-    setValue = function(newValue) { this.Value = newValue }
-    getType  = function()         { return this.Type  }
-    getName  = function()         { return this.Name  }
-    getValue = function()         { return this.Value }
-    isEaten  = function()         { return this.Eaten }
+    
+    setType(newType)  { this.type  = newType  }
+    setName(newName)  { this.name  = newName  }
+    setValue(newValue){ this.value = newValue }
+    getType()         { return this.type  }
+    getName()         { return this.name  }
+    getValue()        { return this.value }
+    isEaten()         { return this.eaten }
 
     //makes the pet eat the food, returns the food's value or 0 if it's already eaten
-    eat() { //oh god please help me this better not all be in here
-        if (this.Eaten) {
-            return 0
+    eat() { 
+        if (this.eaten) {
+            return 0;
         }
-        this.Eaten = true
-        return this.Value
+        this.eaten = true;
+        return this.value;
     }
 
     toString() {
-        let output = ""
-
-        output += this.Name + ": \n"
-        output += "Type: " + this.Type + ", \n"
-        output += "Value: " + this.Value + ", \n"
-        output += "Already Eaten: " + (!this.Eaten && "No" || "Yes") + ", \n"
-        
-        return output
+        let output = "";
+        output += this.name + ": \n";
+        output += "Type: " + this.type + ", \n";
+        output += "Value: " + this.value + ", \n";
+        output += "Already Eaten: " + (!this.eaten && "No" || "Yes") + ", \n";
+        return output;
     }
   
 
 }
+
